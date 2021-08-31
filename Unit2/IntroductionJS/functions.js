7a02db50cc2923a4b9c3747a0b0067b9a37e6a94
Ferain93 <fer.tn7@gmail.com>
2021-08-31 08:53:54 -0500

    feat: executing functions

--- Unit2/IntroductionJS/functions.js
+++ Unit2/IntroductionJS/functions.js
@@ -10,7 +10,14 @@ function mitad(numero){
   return numero/2;
 }
 
-/**/
+/*la función cuadruple que recibe un número y lo multiplica por 4. Ahora te pedimos que debajo de la declaración te encargue de ejecutar esa función, para ello necesitas darle un valor de reemplazo al parámetro, en este caso te pedimos que el valor sea 5. Además queremos que imprimas por consola el resultado de esa ejecución.*/
+
+function cuadruple(numero) {
+  return 4* numero;
+}
+
+console.log(cuadruple(5));
+
 /**/
 /**/
 /**/
