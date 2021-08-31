9b5393fd8a2033d7cc1a8a6b5b1ef8fefe54669d
Ferain93 <fer.tn7@gmail.com>
2021-08-31 00:11:15 -0500

    feat: declare variables with let

---
+++ Unit2/IntroductionJS/Variables.js
@@ -0,0 +1,4 @@
+/*Para comenzar este camino con el pie derecho y empezando a utilizar las buenas prácticas del lenguaje te recomendamos que a partir del próximo ejercicio empieces a declarar tus variables con la palabrareservada let.*/ 
+
+let precio = 27;
+console.log(precio);
