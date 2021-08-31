025495e31e83d71677d423969126f5426e10bf45
Ferain93 <fer.tn7@gmail.com>
2021-08-31 17:06:15 -0500

    feat: prize

--- Unit2/IntroductionJS/conditionalConstructs.js
+++ Unit2/IntroductionJS/conditionalConstructs.js
@@ -110,5 +110,22 @@ function puedeSubirse(alturaPersona, vieneConCompania, tieneAfeccionCardiaca){
   return resultado
 }
 
-/**/
-/**/
+/*¡Te vamos a dar un premio!
+Ya has llegado al nivel necesario para ganarte un premio por tu excelente rendimiento. Ahora que ya sabés lo suficiente de JavaScript, el jurado de un torneo de programación, nos pidió que desarrollemos la función medallaSegunPuesto, la cual retorne la medalla que le corresponde a los primeros puestos, según la siguiente lógica:
+
+Primer puesto: le corresponde "Oro"
+Segundo puesto: le corresponde "Plata"
+Tercer puesto: le corresponde "Bronce"
+Otros puestos: le corresponde "Seguí participando"*/
+
+function medallaSegunPuesto(num){
+  if (num==1){
+        return "Oro"
+  } else if (num==2){
+        return "Plata"
+  } else if (num==3){
+        return "Bronce"
+  } else {
+        return "Seguí participando"
+  }
+}
