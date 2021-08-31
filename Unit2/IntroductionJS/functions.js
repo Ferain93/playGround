91ca935039c7bb5e609f8d4470188ba9932ba705
Ferain93 <fer.tn7@gmail.com>
2021-08-31 08:48:23 -0500

    feat: declaration of functions

---
+++ Unit2/IntroductionJS/functions.js
@@ -0,0 +1,13 @@
+/*Prueba declarando a la función triple, la cual triplique el valor pasado por parámetro y nos devuelva su resultado.*/
+
+function triple(numero) {
+  return 3 * numero;
+}
+
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
