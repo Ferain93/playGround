ec2805a36ab65a212c0e0239b332c68ca728f85c
Ferain93 <fer.tn7@gmail.com>
2021-08-31 16:51:57 -0500

    feat: sign

--- Unit2/IntroductionJS/conditionalConstructs.js
+++ Unit2/IntroductionJS/conditionalConstructs.js
@@ -11,14 +11,29 @@ function sePoneLaOlla(day) {
 /*Definí la función elMayor, la cual debe recibir por parámetro dos números, y como resultado nos devuelva (retorne) el mayor de los dos. Por ejemplo, si la función recibiera 4 y 5 retornaría 5 pues es el mayor, y si pasamos 10 y 4, retornará 10.*/
 
 function elMayor(num1,num2) {
-  if ( num1<num2){
+  if (num1<num2){
       return num2;
   } else {
       return num1;
   }
 }
 
-/**/
+/*Ahora, necesitamos de tus geniales habilidades para que definas la función signo, la cual dado un número, nos retorne solo uno de los siguientes valores:
+
+1, si el número es positivo
+0, si el número es cero
+-1, si el número es negativo*/
+
+function signo(num) {
+  if ( num>1){
+      return 1;
+  } else if (num==0){
+      return 0;
+  } else {
+      return -1;
+  }
+}
+
 /**/
 /**/
 /**/
