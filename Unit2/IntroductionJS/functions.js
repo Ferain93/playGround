bf3cd6f9dde63e6dc1be0f8119825eb0ac1f5ec3
Ferain93 <fer.tn7@gmail.com>
2021-08-31 09:27:18 -0500

    feat: functions concept

--- Unit2/IntroductionJS/functions.js
+++ Unit2/IntroductionJS/functions.js
@@ -24,7 +24,17 @@ function multiplicar(numero1, numero2) {
   return numero1 * numero2;
 }
 
-/**/
+/*Declares una función llamada  tripleDeLaSuma() que reciba dos parámetros. Luego tiene que sumar ambos y devolvernos el triple del valor del resultado de sumar ambos parámetros . Para ello ya contás (aunque no la veas declarada) con la función triple, que recibe un parámetro y retorna el valor del mismo multiplicado por tres.*/
+
+function triple(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+function tripleDeLaSuma(numero1, numero2) {
+  let resultadoDeSuma = triple(numero1, numero2);
+  return resultadoDeSuma * 3;
+}
+
 /**/
 /**/
 /**/
