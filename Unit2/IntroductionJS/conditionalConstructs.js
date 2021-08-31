d4c14e7e4b131de0d2f4b51b9848f418166140cc
Ferain93 <fer.tn7@gmail.com>
2021-08-31 16:47:59 -0500

    feat: what if?

---
+++ Unit2/IntroductionJS/conditionalConstructs.js
@@ -0,0 +1,21 @@
+/*Definí la función sePoneLaOlla, la cual recibe por parámetro el string del día de la semana. Esta función retorna "¡Pongamos la olla, hoy se comen pastas!" si recibe por parámetro el valor "Domingo", caso contrario retornará "Mejor lo dejamos para el domingo"*/
+
+function sePoneLaOlla(day) {
+  if ( day == "Domingo"){
+      return "¡Pongamos la olla, hoy se comen pastas!";
+  } else {
+      return "Mejor lo dejamos para el domingo";
+  }
+}
+
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
