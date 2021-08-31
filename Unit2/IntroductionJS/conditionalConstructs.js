0b5ed784fee32172d459c629f431499529fc5db3
Ferain93 <fer.tn7@gmail.com>
2021-08-31 16:49:26 -0500

    feat: which is greater?

--- Unit2/IntroductionJS/conditionalConstructs.js
+++ Unit2/IntroductionJS/conditionalConstructs.js
@@ -8,7 +8,16 @@ function sePoneLaOlla(day) {
   }
 }
 
-/**/
+/*Definí la función elMayor, la cual debe recibir por parámetro dos números, y como resultado nos devuelva (retorne) el mayor de los dos. Por ejemplo, si la función recibiera 4 y 5 retornaría 5 pues es el mayor, y si pasamos 10 y 4, retornará 10.*/
+
+function elMayor(num1,num2) {
+  if ( num1<num2){
+      return num2;
+  } else {
+      return num1;
+  }
+}
+
 /**/
 /**/
 /**/
