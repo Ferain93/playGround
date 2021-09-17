5488dee23e61ffcb4a5813f837af56166bd2d4d7
Ferain93 <fer.tn7@gmail.com>
2021-09-16 20:00:25 -0500

    feat: array file and favorite series

---
+++ Unit2/IntroductionJS/array.js
@@ -0,0 +1,21 @@
+/*Supongamos que queremos representar al conjunto de nuestras series favoritas. ¿Cómo podríamos hacerlo?
+
+let seriesFavoritasDeAna = ["Game of Thrones", "Breaking Bad", "House of Cards"]; 
+let seriesFavoritasDeHector = ["En Terapia", "Recordando el show de Alejandro Molina"];
+
+Probá y experimenta en la consola las siguientes consultas realizando distintas impresiones por pantalla:
+
+seriesFavoritasDeAna
+seriesFavoritasDeHector
+["hola","mundo!"]
+["hola","hola!"]
+Este ejercicio requiere de que muestres por consola las variables y los arrays mencionados anteriormente.*/
+
+let seriesFavoritasDeAna = ["Game of Thrones", "Breaking Bad", "House of Cards"];
+let seriesFavoritasDeHector = ["En Terapia", "Recordando el Show de Alejandro Molina"]
+let hola=["hola","mundo!"];
+let hola1=["hola","hola!"];
+console.log(seriesFavoritasDeAna);
+console.log(seriesFavoritasDeHector);
+console.log(hola);
+console.log(hola1);
